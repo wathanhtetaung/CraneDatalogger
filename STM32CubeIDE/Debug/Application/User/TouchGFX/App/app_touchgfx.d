09c96e8e6075f730da54ab213bd50255c30f9281
PTS-Wathan <wathan_htet_aung@ptstechnologies.com>
2023-10-22 14:21:22 +0800

    Initial commit

---
+++ STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/TouchGFX/App/app_touchgfx.c \
+ C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/TouchGFX/App/app_touchgfx.h
+C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/TouchGFX/App/app_touchgfx.h:
---
+++ STM32CubeIDE/Debug/Drivers/Components/ft5336.d
@@ -0,0 +1,6 @@
+Drivers/Components/ft5336.o: \
+ C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/Drivers/BSP/Components/ft5336/ft5336.c \
+ C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/Drivers/BSP/Components/ft5336/ft5336.h \
+ C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/Drivers/BSP/Components/ft5336/../Common/ts.h
+C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/Drivers/BSP/Components/ft5336/ft5336.h:
+C:/Users/watha/OneDrive/WorkingPlaces/WathonE/Projects/Datalogger/Drivers/BSP/Components/ft5336/../Common/ts.h:
